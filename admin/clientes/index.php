6320e28763b3cd2490600a2aeb1e2caf3c3a0557
UiraGarro <extadexavier@gmail.com>
2025-11-19 20:00:24 -0300

    feat: CRUD cliente

---
+++ admin/clientes/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tem Quase Tudo - Cliente</title>
+</head>
+<body>
+
+<h1>Painel de Administração de Cliente</h1>
+
+<ul>
+    <li><a href="cadastrar_cliente.php">Adicionar Cliente</a></li>
+    <li><a href="listar_cliente.php">Listar Cliente</a></li>
+</ul>
+
+    <li><a href="../index.php">Voltar ao Painel Principal</a></li>
+</body>
+</html>
